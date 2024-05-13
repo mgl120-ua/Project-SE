6bafaeaec195aecb0aab1bcecf985bacc8be4091
Angel <angel.briones.munoz@alumnos.upm.es>
2024-05-13 22:11:04 +0200

    tecnical errors

--- src/main/java/pl/put/poznan/transformer/logic/ScenarioQualityChecker.java
+++ src/main/java/pl/put/poznan/transformer/logic/ScenarioQualityChecker.java
@@ -1,8 +1,54 @@
 package pl.put.poznan.transformer.logic;
 
 public class ScenarioQualityChecker {
-    String Scenario;
-    String Actor;
-    String SystemActor;
-    String[] Steps;
+    private String scenario;       // Escenario
+    private String actor;          // Actor
+    private String systemActor;    // Actor del sistema
+    private String[] steps;        // Pasos del escenario
+
+    // Constructor vacío
+    public ScenarioQualityChecker() {
+    }
+
+    // Constructor con todos los campos
+    public ScenarioQualityChecker(String scenario, String actor, String systemActor, String[] steps) {
+        this.scenario = scenario;
+        this.actor = actor;
+        this.systemActor = systemActor;
+        this.steps = steps;
+    }
+
+    // Getters
+    public String getScenario() {
+        return scenario;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public String getSystemActor() {
+        return systemActor;
+    }
+
+    public String[] getSteps() {
+        return steps;
+    }
+
+    // Setters
+    public void setScenario(String scenario) {
+        this.scenario = scenario;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public void setSystemActor(String systemActor) {
+        this.systemActor = systemActor;
+    }
+
+    public void setSteps(String[] steps) {
+        this.steps = steps;
+    }
 }
--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -15,7 +15,7 @@ public class TextTransformerController {
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+                      @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
         // log the parameters
         logger.debug(text);
@@ -28,7 +28,7 @@ public class TextTransformerController {
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
+                       @RequestBody String[] transforms) {
 
         // log the parameters
         logger.debug(text);
